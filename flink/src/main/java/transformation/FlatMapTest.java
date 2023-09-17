c879968e6b19701409e345a761def02d026482b8
junjilai <945700840@qq.com>
2023-09-17 19:41:16 +0800

    [feature] add flatmap operator examples

--- flink/src/main/java/transformation/FlatMapTest.java
+++ flink/src/main/java/transformation/FlatMapTest.java
@@ -1,4 +1,30 @@
 package transformation;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
 public class FlatMapTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<String> elements = env.fromElements("a b c","a c d","k f c");
+
+        elements.flatMap(new FlatMapFunction<String, String>() {
+
+            /**
+             * @param s
+             * @param out
+             * @throws Exception
+             */
+            public void flatMap(String s, Collector<String> out) throws Exception {
+                for(String e: s.split(" ")){
+                    out.collect(e);
+                }
+            }
+        }).print();
+
+        env.execute();
+    }
 }
