e2e8fd93c6ac5e8c5f9faf58164be492d682d0de
junjilai <945700840@qq.com>
2023-09-18 22:17:56 +0800

    [feature] add filter operator examples

--- flink/src/main/java/transformation/FilterTest.java
+++ flink/src/main/java/transformation/FilterTest.java
@@ -1,4 +1,21 @@
 package transformation;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class FilterTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Integer> elements = env.fromElements(1,2,3,4,5);
+
+        elements.filter(new FilterFunction<Integer>() {
+            public boolean filter(Integer num) throws Exception {
+                return num % 2 == 0 ? false : true;
+            }
+        }).print();
+
+        env.execute();
+    }
 }
